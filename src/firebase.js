befaf95910ebb2b5f5a2b359c4e7039f1a507f52
Nyumbe <wilsonnyumbe99@gmail.com>
2024-11-10 19:00:11 +0300

    fixed bug

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
     projectId: process.env.REACT_APP_PROJECTID,
     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
